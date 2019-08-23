2eddff1988386b4149ca0e825a48d9a84442d362
lucaspellegrinelli <lucas.pellegrinelli@hotmail.com>
2019-08-23 13:08:40 -0300

    Identation test

--- run.py
+++ run.py
@@ -2,39 +2,39 @@ from monopoly.game import Game
 from bots.random_ai import RandomAI
 
 if __name__ == '__main__':
-	
-	# Creates and runs a game with 5 'RandomAI' bots
 
-	players = [
-		{
-			"name": "Bot 1",
-			"ai": RandomAI()
-		},
-		{
-			"name": "Bot 2",
-			"ai": RandomAI()
-		},
-		{
-			"name": "Bot 3",
-			"ai": RandomAI()
-		},
-		{
-			"name": "Bot 4",
-			"ai": RandomAI()
-		},
-		{
-			"name": "Bot 5",
-			"ai": RandomAI()
-		},
-	]
+  # Creates and runs a game with 5 'RandomAI' bots
 
-	game = Game(players)
+  players = [
+    {
+      "name": "Bot 1",
+      "ai": RandomAI()
+    },
+    {
+      "name": "Bot 2",
+      "ai": RandomAI()
+    },
+    {
+      "name": "Bot 3",
+      "ai": RandomAI()
+    },
+    {
+      "name": "Bot 4",
+      "ai": RandomAI()
+    },
+    {
+      "name": "Bot 5",
+      "ai": RandomAI()
+    },
+  ]
 
-	result = game.run()
+  game = Game(players)
 
-	print(" ----------------------------------------- ")
-	if result.draw:
-		print("\tThe game ended on a draw")
-	else:
-		print("\tThe winner was " + str(result.player.name))
-	print(" ----------------------------------------- ")
+  result = game.run()
+
+  print(" ----------------------------------------- ")
+  if result.draw:
+    print("\tThe game ended on a draw")
+  else:
+    print("\tThe winner was " + str(result.player.name))
+  print(" ----------------------------------------- ")
